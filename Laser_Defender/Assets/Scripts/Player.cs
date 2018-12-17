d3618d92db76c619ed2e82ae488b64ae2e3865eb
thanh1985tq <vcthanhtq@gmail.com>
2018-12-17 23:03:19 +0700

    Add enemies sprites + add scripts for Player
    Add enemies sprites + add scripts for Player

--- Laser_Defender/Assets/Scripts/Player.cs
+++ Laser_Defender/Assets/Scripts/Player.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] GameObject playerProjectile;
+    [SerializeField] float projectileSpeed = 5f;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float fireRate = 0.2f;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera myCamera = Camera.main;
+        minX = myCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        maxX = myCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        minY = myCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        maxY = myCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(FireContinuosly());
+        }
+    }
+
+    private IEnumerator FireContinuosly()
+    {
+        while (true)
+        {
+            GameObject projectile = Instantiate(playerProjectile, transform.position, Quaternion.identity) as GameObject;
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+
+    private void Move()
+    {
+        float xOffset = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yOffset = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float xRange = Mathf.Clamp(xOffset + transform.position.x, minX, maxX);
+        float yRange = Mathf.Clamp(yOffset + transform.position.y, minY, maxY);
+        transform.position = new Vector2(xRange, yRange);
     }
 }
