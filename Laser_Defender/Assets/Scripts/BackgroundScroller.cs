3a111785de9d8fc015b0d104d5cc3bfd9f5c4a97
thanh1985tq <vcthanhtq@gmail.com>
2018-12-17 12:59:43 +0700

    Add BG scroller
    - Add background scroller
    - Add sprites player and enemy to the scene

---
+++ Laser_Defender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    //Config parameters
+    [SerializeField] float scrollSpeed = 0.02f;
+
+    //Local parameters
+    Vector2 offSet;
+    Material myMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, scrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
